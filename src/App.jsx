f709d657213121a21e1eb62e0b580f76075f5349
Anshima <anshima541@gmail.com>
2024-07-27 16:44:54 +0530

    footer complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from './Components/About';
 import Eyes from './Components/Eyes';
 import Featured from './Components/Featured';
 import Cards from './Components/Cards';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
      <Eyes/>
      <Featured/>
      <Cards/>
+     <Footer/>
     </div>
   )
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className='relative flex w-full h-screen bg-zinc-900 p-20'>
+      <div className='w-1/2 h-full flex flex-col justify-between font-["Founders_Grotesk_X-Condensed"]'>
+        <div className="heading">
+          <h1 className='text-[6vw] font-semibold uppercase leading-none mb-5'>EYE-</h1>
+          <h1 className='text-[6vw] font-semibold uppercase leading-none'>OPENING</h1>
+        </div>
+        {/* SVG positioned absolutely at the bottom left corner */}
+        <svg 
+          className='absolute bottom-0 left-0 mb-5 ml-5' 
+          width="72" 
+          height="30" 
+          viewBox="0 0 72 30" 
+          fill="none" 
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          {/* SVG paths */}
+        </svg>
+      </div>
+      <div className="w-1/2">
+        <h1 className='text-[4vw] font-semibold uppercase leading-none mb-10'>PRESENTATIONS</h1>
+        <div className="dets font-['Neue_Montreal']">
+            <a href="#" className="block text-xl font-light mb-2">Facebook</a>
+            <a href="#" className="block text-xl font-light mb-2">Instagram</a>
+            <a href="#" className="block text-xl font-light">Twitter</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
