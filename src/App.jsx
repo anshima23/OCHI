888dc2dae408cd7ec7ad8bb700125ff20c39e026
Anshima <anshima541@gmail.com>
2024-07-27 11:18:19 +0530

    marquee ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
 import LandingPage from './Components/LandingPage';
+import Marquee from './Components/Marquee';
 
 function App() {
   return (
     <div className='w-full h-screen bg-zinc-900 text-white'>
      <Navbar/>
      <LandingPage/>
+     <Marquee/>
     </div>
   )
 }
---
+++ src/Components/Marquee.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+function Marquee() {
+  return (
+    <div className='w-full bg-[#004D43] h-[25rem]'>
+      <div className='flex gap-10 overflow-hidden whitespace-nowrap h-full items-center'>
+        {["We are ochi", "We are ochi", "We are ochi"].map((text, index) => (
+          <motion.div 
+            key={index}
+            initial={{x:0}} 
+            animate={{x:"-100%"}} 
+            transition={{ease:"linear", repeat:Infinity, duration:5}} 
+            className='flex items-center border-t-2 border-b-2 border-zinc-300'
+          >
+            <h1 className='text-[17vw] leading-none font-["Founders_Grotest_X-Condensed"] uppercase px-5 py-2 font-semibold'>
+              {text}
+            </h1>
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Marquee;
